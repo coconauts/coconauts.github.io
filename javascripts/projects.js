6961b6644ebe9bb5674f2fda886b02a9072a9855
Mar Bartlome <mar@lystable.com>
2016-10-05 23:09:02 +0100

    Site updated at 2016-10-05 22:09:01 UTC

--- javascripts/projects.js
+++ javascripts/projects.js
@@ -1,29 +1,21 @@
-//if you add a new status , add a color in projects.css with the correct name
-var progress = "In progress",
-    released = "Released",
-    deprecated = "Deprecated";
-
 function getIcon(tag){
     return "<a target='_self' href='/projects/"+tag+"' class='circular  project-"+tag+"'></a>";
 }
 
-function getStatusTag(status){
-    return status.toLowerCase().replace(/\s/g,"-");
-}
-function add(tag, year, status, title, desc, tech ){
-    
+function add(tag, year, type, title, desc, tech ){
+
    var icon = getIcon(tag);
-   
-    addProject(icon, year, status, title, desc, tech );
+
+    addProject(icon, year, type, title, desc, tech );
     addProjectByYear(icon, year );
-    addProjectByStatus(icon, status );
+    addProjectByCategory(icon, type );
 }
-function addProject(icon, year, status, title, desc, tech ){
-    
-    var project = 
+function addProject(icon, year, type, title, desc, tech ){
+
+    var project =
     "<tr><td class='iconCell'> "+icon+"</td>"+
         "<td class='year'>"+year+"</td>"+
-        "<td class='status "+getStatusTag(status)+"'>"+status+"</td>"+
+        "<td class='type "+type+"'>"+type+"</td>"+
         "<td class='description'>"+
             "<p class='title'>"+title+"</p>"+
             "<p>"+desc+"</p>"+
@@ -34,20 +26,18 @@ function addProject(icon, year, status, title, desc, tech ){
 }
 
 function addProjectByYear(icon, year){
-    if ($("#projectYear").find("."+year).length == 0 ) 
+    if ($("#projectYear").find("."+year).length == 0 )
         $("#projectYear").append("<div class='category "+year+"' >"+
             "<p class='gray'>"+year+"</p></div>");
-    
-    $("#projectYear").find("."+year).append(icon);
-    
-}
-function addProjectByStatus(icon, status){
-    var className = getStatusTag(status);
-    if ($("#projectStatus").find("."+className).length == 0 ) 
-        $("#projectStatus").append("<div class='category "+className+"' >"+
-            "<p >"+status+"</p></div>");
-    
-    $("#projectStatus").find("."+className).append(icon);
-    
-}
 
+    $("#projectYear").find("."+year).append(icon);
+
+}
+function addProjectByCategory(icon, type){
+    if ($("#projectStatus").find("."+type).length == 0 )
+        $("#projectStatus").append("<div class='category "+type+"' >"+
+            "<p >"+type+"</p></div>");
+
+    $("#projectStatus").find("."+type).append(icon);
+
+}
